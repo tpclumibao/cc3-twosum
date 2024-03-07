3cf2cf24359dae18b959535cc8ed173739ab1401
Lumibao, Tom Peycee C <145944398+tpclumibao@users.noreply.github.com>
2024-03-07 04:20:02 -0800

    Add files via upload

---
+++ TwoSum/MainFIle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoSum
+{
+    class MainFIle
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the numbers separated by space = ");
+            int[] nums = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            Console.Write("Target = ");
+            int target = int.Parse(Console.ReadLine());
+
+            Solution solution = new Solution();
+            int[] result = solution.TwoSum(nums, target);
+            if (result.Length > 0)
+            {
+                Console.Write("Indices = [{0}]", string.Join(", ", result));
+            }
+            else
+            {
+                Console.Write("No solution found.");
+            }
+        }
+    }
+}
+
---
+++ TwoSum/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            List<(int num, int index)> numIndices = new List<(int num, int index)>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                numIndices.Add((nums[i], i));
+            }
+
+            for (int i = 0; i < numIndices.Count; i++)
+            {
+                int complement = target - numIndices[i].num;
+
+                for (int j = i + 1; j < numIndices.Count; j++)
+                {
+                    if (numIndices[j].num == complement)
+                    {
+                        return new int[] { numIndices[i].index, numIndices[j].index };
+                    }
+                }
+            }
+
+            return new int[0];
+        }
+    }
+}
